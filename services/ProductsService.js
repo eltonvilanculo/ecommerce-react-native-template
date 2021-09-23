e47005ba453601bc7fa0192415a94ef38332c4c1
dinaldomanjate <dinaldomajate@gmail.com>
2021-09-23 10:44:28 +0200

    produtos

--- services/ProductsService.js
+++ services/ProductsService.js
@@ -15,11 +15,46 @@ const PRODUCTS = [
     },
     {
         id: 102,
-        name: 'Bolo no pote',
+        name: 'Bolo pote',
         price: 450,
-        image: require('../assets/products/cake-102.jpg'),
+        image: require('../assets/products/Bolo-no-pote.jpg'),
         description: 'Bom produto, com um alcance excelente e garantia inclusa'
-    }
+    },
+    {
+        id: 103,
+        name: 'Iphone 13',
+        price: 110000,
+        image: require('../assets/products/iphone-13.jpg'),
+        description: 'O Ultimo modelo do iphone disponivel'
+    },
+    {
+        id: 104,
+        name: 'Jantes 18"',
+        price: 50000,
+        image: require('../assets/products/Jantes.jpg'),
+        description: 'O melhor tipo de jante disponivel ao melhor preco'
+    },
+    {
+        id: 105,
+        name: 'Carros',
+        price: 350000,
+        image: require('../assets/products/Toyota-Ractis.jpg'),
+        description: 'Toyota Ractis disponivel ao melhor preco'
+    },
+    {
+        id: 106,
+        name: 'Pneus',
+        price: 30000,
+        image: require('../assets/products/Pneus.jpg'),
+        description: 'Todo o tipo de pneus e quase toda as referencias disponiveis'
+    },
+    {
+        id: 107,
+        name: 'Bolos',
+        price: 1500,
+        image: require('../assets/products/Bolo.jpg'),
+        description: 'A Lolita bolos fornece bolos de melhor qualidade '
+    },
 ];
 
 export function getProducts() {
